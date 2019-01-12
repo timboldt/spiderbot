dad7b4be4e6705a02c985c240017c3a69352632d
Tim Boldt <tim.boldt@gmail.com>
2019-01-12 15:48:11 -0800

    Got basic coxa rotation working

---
+++ spider.py
@@ -0,0 +1,59 @@
+#! /usr/bin/env python
+
+import math
+import time
+
+import ssc32
+import spider_leg
+
+ssc = ssc32.SSC32('/dev/ttyS3', 115200, count=32)
+
+# Back Right
+br_tibia = ssc[0]
+br_femur = ssc[1]
+br_coxa = ssc[2]
+backwards=True
+
+# # Front Right
+# br_tibia = ssc[15]
+# br_femur = ssc[14]
+# br_coxa = ssc[13]
+# backwards=False
+
+# # Front Left
+# br_tibia = ssc[31]
+# br_femur = ssc[30]
+# br_coxa = ssc[29]
+# backwards=True
+
+# # Back Left
+# br_tibia = ssc[16]
+# br_femur = ssc[17]
+# br_coxa = ssc[18]
+# backwards=False
+
+br_leg = spider_leg.SpiderLeg(backwards=backwards)
+
+br_leg.x = 70
+br_leg.y = 20
+#br_leg.z = 27
+(br_tibia.degrees, br_femur.degrees, br_coxa.degrees) = br_leg.get_servo_angles()
+ssc.commit(time=1000)
+while not ssc.is_done():
+    time.sleep(0.1)
+
+br_leg.x = 20
+br_leg.y = 70
+#br_leg.z = -50
+(br_tibia.degrees, br_femur.degrees, br_coxa.degrees) = br_leg.get_servo_angles()
+ssc.commit(time=1000)
+while not ssc.is_done():
+    time.sleep(0.1)
+
+br_leg.x = 34
+br_leg.y = 34
+#br_leg.z = -50
+(br_tibia.degrees, br_femur.degrees, br_coxa.degrees) = br_leg.get_servo_angles()
+ssc.commit(time=1000)
+while not ssc.is_done():
+    time.sleep(0.1)
--- spider_leg.py
+++ spider_leg.py
@@ -1,23 +1,26 @@
 import math
 
 class SpiderLeg:
-    def __init__(self):
-        self._coxa_len = 27.5
-        self._femur_len = 70
+    def __init__(self, backwards):
+        self._coxa_len = 24
+        self._femur_len = 38
         self._tibia_len = 80
+        self._backwards = backwards
 
         xy_start = self._coxa_len + self._femur_len * 0.55
         (self.x, self.y, self.z) = (xy_start, xy_start, -50)
 
     def get_servo_angles(self):
-        if (self.x >= 0):
-            w = math.sqrt(pow(self.x, 2) + pow(self.y, 2))
-        else:
-            w = -1 * (math.sqrt(pow(self.x, 2) + pow(self.y, 2)))
+        assert self.x >= 0 and self.x <= 100
+        assert self.y >= 0 and self.y <= 100
 
+        # Full distance to reach.
+        w = math.sqrt(pow(self.x, 2) + pow(self.y, 2))
+
+        # Distance to reach from the end of the coxa.
         v = w - self._coxa_len
-        alpha_tmp = (pow(self._femur_len, 2) - pow(self._tibia_len, 2) + pow(v, 2) + pow(self.z, 2)) / 2 / self._femur_len / math.sqrt(
-            pow(v, 2) + pow(self.z, 2))
+
+        alpha_tmp = (pow(self._femur_len, 2) - pow(self._tibia_len, 2) + pow(v, 2) + pow(self.z, 2)) / 2 / self._femur_len / math.sqrt(pow(v, 2) + pow(self.z, 2))
         if (alpha_tmp > 1 or alpha_tmp < -1):
             print("x=%f y=%f v=%f w=%f" % (self.x, self.y, v, w))
             print("alpha=%f" % alpha_tmp)
@@ -37,14 +40,18 @@ class SpiderLeg:
                 beta_tmp = -1
         beta = math.acos(beta_tmp)
 
-        if (w >= 0):
-            gamma = math.atan2(self.y, self.x)
-        else:
-            gamma = math.atan2(-self.y, -self.x)
-        return (alpha*180/math.pi, beta*180/math.pi, gamma*180/math.pi)
+        # Coxa angle.
+        gamma = math.atan2(self.y, self.x)
+        if self._backwards:
+            gamma = -gamma
+
+        return (alpha*180/math.pi-50, beta*180/math.pi-50, gamma*180/math.pi)
 
 if __name__ == "__main__":
-    leg = SpiderLeg()
+    leg = SpiderLeg(backwards=False)
     print(leg.get_servo_angles())
-    leg.z = 0
+    leg.z = 50
+    print(leg.get_servo_angles())
+    leg.x = 70
+    leg.y = 20
     print(leg.get_servo_angles())
