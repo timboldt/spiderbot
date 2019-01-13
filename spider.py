87b15f1fc129f2e767bbae5239dd761f29f1db1a
Tim Boldt <tim.boldt@gmail.com>
2019-01-12 22:51:07 -0800

    Added alpha (femur-tibia) angle calculations.

--- spider.py
+++ spider.py
@@ -12,7 +12,7 @@ ssc = ssc32.SSC32('/dev/ttyS3', 115200, count=32)
 br_tibia = ssc[0]
 br_femur = ssc[1]
 br_coxa = ssc[2]
-backwards=True
+backwards = True
 
 # # Front Right
 # br_tibia = ssc[15]
@@ -26,7 +26,7 @@ backwards=True
 # br_coxa = ssc[29]
 # backwards=True
 
-# # Back Left
+# Back Left
 # br_tibia = ssc[16]
 # br_femur = ssc[17]
 # br_coxa = ssc[18]
@@ -37,7 +37,9 @@ br_leg = spider_leg.SpiderLeg(backwards=backwards)
 br_leg.x = 70
 br_leg.y = 20
 #br_leg.z = 27
-(br_tibia.degrees, br_femur.degrees, br_coxa.degrees) = br_leg.get_servo_angles()
+a, b, g = br_leg.get_servo_angles()
+print(a, b, g)
+(br_tibia.degrees, br_femur.degrees, br_coxa.degrees) = (a, b, g)
 ssc.commit(time=1000)
 while not ssc.is_done():
     time.sleep(0.1)
@@ -45,15 +47,29 @@ while not ssc.is_done():
 br_leg.x = 20
 br_leg.y = 70
 #br_leg.z = -50
-(br_tibia.degrees, br_femur.degrees, br_coxa.degrees) = br_leg.get_servo_angles()
+a, b, g = br_leg.get_servo_angles()
+print(a, b, g)
+(br_tibia.degrees, br_femur.degrees, br_coxa.degrees) = (a, b, g)
 ssc.commit(time=1000)
 while not ssc.is_done():
     time.sleep(0.1)
 
-br_leg.x = 34
-br_leg.y = 34
+br_leg.x = 70
+br_leg.y = 70
 #br_leg.z = -50
-(br_tibia.degrees, br_femur.degrees, br_coxa.degrees) = br_leg.get_servo_angles()
+a, b, g = br_leg.get_servo_angles()
+print(a, b, g)
+(br_tibia.degrees, br_femur.degrees, br_coxa.degrees) = (a, b, g)
+ssc.commit(time=1000)
+while not ssc.is_done():
+    time.sleep(0.1)
+
+br_leg.x = 44
+br_leg.y = 44
+#br_leg.z = -50
+a, b, g = br_leg.get_servo_angles()
+print(a, b, g)
+(br_tibia.degrees, br_femur.degrees, br_coxa.degrees) = (a, b, g)
 ssc.commit(time=1000)
 while not ssc.is_done():
     time.sleep(0.1)
--- spider_leg.py
+++ spider_leg.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python
+
 import math
 
 class SpiderLeg:
@@ -14,44 +16,61 @@ class SpiderLeg:
         assert self.x >= 0 and self.x <= 100
         assert self.y >= 0 and self.y <= 100
 
-        # Full distance to reach.
-        w = math.sqrt(pow(self.x, 2) + pow(self.y, 2))
+        # Full horizontal distance (i.e., in the XY plane) from body to target.
+        xy_total = math.sqrt(self.x * self.x + self.y * self.y)
 
-        # Distance to reach from the end of the coxa.
-        v = w - self._coxa_len
+        # Horizontal distance (i.e., in the XY plane) to reach from the end of the coxa.
+        xy_reach = xy_total - self._coxa_len
 
-        alpha_tmp = (pow(self._femur_len, 2) - pow(self._tibia_len, 2) + pow(v, 2) + pow(self.z, 2)) / 2 / self._femur_len / math.sqrt(pow(v, 2) + pow(self.z, 2))
-        if (alpha_tmp > 1 or alpha_tmp < -1):
-            print("x=%f y=%f v=%f w=%f" % (self.x, self.y, v, w))
-            print("alpha=%f" % alpha_tmp)
-            if (alpha_tmp > 1):
-                alpha_tmp = 1
-            else:
-                alpha_tmp = -1
-        alpha = math.atan2(self.z, v) + math.acos(alpha_tmp)
+        # Absolute distance from coxa to tibia tip.
+        xyz_reach = math.sqrt(xy_reach * xy_reach + self.z * self.z)
+        assert xyz_reach > 60 and xyz_reach < 110
 
-        beta_tmp = (pow(self._femur_len, 2) + pow(self._tibia_len, 2) - pow(v, 2) - pow(self.z, 2)) / 2 / self._femur_len / self._tibia_len
-        if (beta_tmp > 1 or beta_tmp < -1):
-            print("x=%f y=%f v=%f w=%f" % (self.x, self.y, v, w))
-            print("beta=%f" % beta_tmp)
-            if (beta_tmp > 1):
-                beta_tmp = 1
-            else:
-                beta_tmp = -1
-        beta = math.acos(beta_tmp)
+        # We now have a triangle with sides [femur_len, tibia_len, xyz_reach].
+        # Solve using the law of cosines.
+        cos_alpha = (
+            self._femur_len * self._femur_len
+            + self._tibia_len * self._tibia_len
+            - xyz_reach * xyz_reach
+            ) / (
+            2.0 * self._femur_len * self._tibia_len
+            )
+        alpha = math.acos(cos_alpha) * 180.0 / math.pi - 90
+
+        # beta_tmp = (pow(self._femur_len, 2) + pow(self._tibia_len, 2) - pow(v, 2) - pow(self.z, 2)) / 2 / self._femur_len / self._tibia_len
+        # if (beta_tmp > 1 or beta_tmp < -1):
+        #     print("x=%f y=%f v=%f w=%f" % (self.x, self.y, v, w))
+        #     print("beta=%f" % beta_tmp)
+        #     if (beta_tmp > 1):
+        #         beta_tmp = 1
+        #     else:
+        #         beta_tmp = -1
+        # beta = math.acos(beta_tmp)
+        beta = 0
 
         # Coxa angle.
-        gamma = math.atan2(self.y, self.x)
+        gamma = math.atan2(self.y, self.x) * 180.0 / math.pi
         if self._backwards:
+            alpha = -alpha
             gamma = -gamma
 
-        return (alpha*180/math.pi-50, beta*180/math.pi-50, gamma*180/math.pi)
+        return (alpha, beta, gamma)
 
 if __name__ == "__main__":
     leg = SpiderLeg(backwards=False)
-    print(leg.get_servo_angles())
+    a,b,g = leg.get_servo_angles()
+    print("x=%f y=%f z=%f, a=%f, b=%f, g=%f" % (leg.x, leg.y, leg.z, a, b, g))
+
     leg.z = 50
-    print(leg.get_servo_angles())
+    a,b,g = leg.get_servo_angles()
+    print("x=%f y=%f z=%f, a=%f, b=%f, g=%f" % (leg.x, leg.y, leg.z, a, b, g))
+
     leg.x = 70
     leg.y = 20
-    print(leg.get_servo_angles())
+    a,b,g = leg.get_servo_angles()
+    print("x=%f y=%f z=%f, a=%f, b=%f, g=%f" % (leg.x, leg.y, leg.z, a, b, g))
+
+    leg.x = 80
+    leg.y = 80
+    a,b,g = leg.get_servo_angles()
+    print("x=%f y=%f z=%f, a=%f, b=%f, g=%f" % (leg.x, leg.y, leg.z, a, b, g))
