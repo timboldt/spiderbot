63713eb4e44a5ce3b02ff8acbe74f300f32c45a2
Tim Boldt <tim.boldt@gmail.com>
2019-01-17 21:47:38 -0800

    Got body shifting and leg weight shifting working

--- spider_body.py
+++ spider_body.py
@@ -5,6 +5,11 @@ import spider_leg as leg
 import ssc32
 import time
 
+# Legs are:
+#   0 - Back right
+#   1 - Front right
+#   2 - Front left
+#   3 - Back left
 class SpiderBody:
     def __init__(self, ssc):
         self._ssc = ssc
@@ -12,11 +17,16 @@ class SpiderBody:
         self._tibia_servos = [0, 15, 31, 16]
         self._femur_servos = [1, 14, 30, 17]
         self._coxa_servos = [2, 13, 29, 18]
+        self.BACK_RIGHT = 0
+        self.FRONT_RIGHT = 1
+        self.FRONT_LEFT = 2
+        self.BACK_LEFT = 3
+        self.commit_speed = 10
 
     def _commit(self, millis):
         for i in range(4):
             a, b, g = self.legs[i].get_servo_angles()
-            if i % 2 == 1:
+            if i == self.BACK_LEFT or i == self.FRONT_RIGHT:
                 self._ssc[self._tibia_servos[i]].degrees = a - 90
                 self._ssc[self._femur_servos[i]].degrees = b - 180
                 self._ssc[self._coxa_servos[i]].degrees = 90 - g
@@ -24,36 +34,42 @@ class SpiderBody:
                 self._ssc[self._tibia_servos[i]].degrees = 90 - a
                 self._ssc[self._femur_servos[i]].degrees = 180 - b
                 self._ssc[self._coxa_servos[i]].degrees = g - 90
-        self._ssc.commit(millis)
+        self._ssc.commit(millis*self.commit_speed)
         while not self._ssc.is_done():
             time.sleep(0.1)
     
     def stand_up(self):
         for i in range(4):
             (self.legs[i].x, self.legs[i].y, self.legs[i].z) = self.legs[i].get_start_xyz()
-        self._commit(1000)
+        self._commit(100)
+
+    def sit_down(self):
+        for i in range(4):
+            (self.legs[i].x, self.legs[i].y, self.legs[i].z) = self.legs[i].get_start_xyz()
+            self.legs[i].z += 30
+        self._commit(100)
 
     def lift_leg(self, id, mm=20):
         self.legs[id].z += mm
-        self._commit(1000)
+        self._commit(100)
 
-    def shift_weight_off_leg(self, id, mm=10):
-        if i % 2 == 1:
-            mm = -mm
-        self.legs[id].x += mm
-        self.legs[id].y += mm
-        self.legs[(id+1)%4].x += mm * 0.7
-        self.legs[(id+1)%4].y -= mm * 0.7
-        self.legs[(id+2)%4].x -= mm
-        self.legs[(id+2)%4].y -= mm
-        self.legs[(id+3)%4].x -= mm * 0.7
-        self.legs[(id+3)%4].y += mm * 0.7
-        self._commit(1000)
+    def shift_body(self, x_mm, y_mm):
+        self.legs[self.FRONT_LEFT].x += x_mm
+        self.legs[self.FRONT_LEFT].y -= y_mm
+        self.legs[self.FRONT_RIGHT].x -= x_mm
+        self.legs[self.FRONT_RIGHT].y -= y_mm
+        self.legs[self.BACK_LEFT].x += x_mm
+        self.legs[self.BACK_LEFT].y += y_mm
+        self.legs[self.BACK_RIGHT].x -= x_mm
+        self.legs[self.BACK_RIGHT].y += y_mm
+        self._commit(100)
 
-if __name__ == "__main__":
-    body = SpiderBody(ssc32.SSC32('/dev/ttyS6', 9600, count=32))
-    body.stand_up()
-    for i in range(4):
-        body.shift_weight_off_leg(i)
-        body.lift_leg(i)
-        body.stand_up()
+    def shift_weight_off_leg(self, id, mm=20):
+        if id == self.FRONT_LEFT:
+            self.shift_body(mm * 0.7, mm * -0.7)
+        elif id == self.FRONT_RIGHT:
+            self.shift_body(mm * -0.7, mm * -0.7)
+        elif id == self.BACK_LEFT:
+            self.shift_body(mm * 0.7, mm * 0.7)
+        else: # BACK_RIGHT
+            self.shift_body(mm * -0.7, mm * 0.7)
