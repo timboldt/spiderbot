debfb677f34901ca5d20b31d09dc53862ab69b00
Tim Boldt <tim.boldt@gmail.com>
2020-06-27 14:29:13 -0700

    Moved old python code out of the way

--- original_python/spider.py
+++ original_python/spider.py
@@ -7,14 +7,18 @@ import time
 import ssc32
 import spider_body
 
-ssc = ssc32.SSC32('/dev/ttyS6', 9600, count=32)
+ssc = ssc32.SSC32('/dev/rfcomm0', 9600, count=32)
 body = spider_body.SpiderBody(ssc)
-body.stand_up()
+# for i in range(4):
+#     body.stand_up()
+#     time.sleep(1)
+#     body.sit_down()
+#     time.sleep(1)
 for i in range(4):
     body.shift_weight_off_leg(i)
     body.leg_up(i, 40)
     body.leg_down(i)
     body.stand_up()
-body.walk()
+#body.walk()
 #time.sleep(3)
 body.sit_down()
