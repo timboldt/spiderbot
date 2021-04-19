845ef41add1cd865ca0364a5d27db6fe9e014e6e
Tim Boldt <tim.boldt@gmail.com>
2021-04-18 21:35:02 -0700

    Fixed bug in coxa-femur joint direction.

--- arduino/src/spider/config_servos.cpp
+++ arduino/src/spider/config_servos.cpp
@@ -22,25 +22,25 @@ Servo servo_config[12] = {
     // FR BC
     Servo(1500, 2500, 1700, false),
     // FR CF
-    Servo(1200, 2600, 2111, false),
+    Servo(1200, 2600, 2111, true),
     // FR FT
     Servo(1400, 2500, 900, false),
     // FL BC
     Servo(700, 1700, -400, false),
     // FL CF
-    Servo(500, 1900, 1045, true),
+    Servo(500, 1900, 1045, false),
     // FL FT
     Servo(1300, 2400, 2800, true),
     // BR BC
     Servo(700, 1700, 1800, false),
     // BR CF
-    Servo(700, 2100, 1189, true),
+    Servo(700, 2100, 1189, false),
     // BR FT
     Servo(1500, 2500, 3100, true),
     // BL BC
     Servo(1400, 2400, 3500, false),
     // BL CF
-    Servo(1000, 2200, 1600, false),
+    Servo(1000, 2200, 1600, true),
     // BL FT
     Servo(1100, 2200, 600, false),
 };
